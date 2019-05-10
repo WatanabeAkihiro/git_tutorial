c46d4f8e12e75a63b2baf4a1026db2dbb86dd067
Tanisuke Goro <keisi1100525@outlook.jp>
2019-05-10 18:44:57 +0900

    セミコロンを忘れてた.

--- js/main.js
+++ js/main.js
@@ -4,8 +4,8 @@ const $id = (id) => document.getElementById(id);
 $('button').click(function() {
     console.log('クリックされました！');
 
-    const arg1 = $id('arg1').value
-    const arg2 = $id('arg2').value
+    const arg1 = $id('arg1').value;
+    const arg2 = $id('arg2').value;
     const arg = (arg2 !== 0) ? arg1 / arg2 : "ゼロでは割れません";
     $('#output-area').html(`<p>${arg}</p>`)
 })
